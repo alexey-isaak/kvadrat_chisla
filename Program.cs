a9d46c56ed39bd2b62c9b0db40cf599a77f9955f
Alexey_I <gafar@xaker.ru>
2023-02-03 07:32:42 +0300

    Itogovy sxript

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ 
+namespace nomer_dnya_nedeli
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите первое число: ");
+            int chislo1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int chislo2 = int.Parse(Console.ReadLine());
+            if ( chislo1 == chislo2 * chislo2 )
+            {
+                Console.WriteLine(chislo1 + " является квадратом числа " + chislo2);
+            }
+            else
+            {
+                Console.WriteLine(chislo1 + " НЕ является квадратом числа " + chislo2);
+            }
+        }
+    }
+}
